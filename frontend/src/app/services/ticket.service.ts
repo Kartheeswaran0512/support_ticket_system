2f81907825a8d899987bb51dc49aa4cee0d2ea46
kartheeswaran <karthi@gmail.com>
2025-07-03 13:46:10 +0530

    addded backend link

--- frontend/src/app/services/ticket.service.ts
+++ frontend/src/app/services/ticket.service.ts
@@ -5,7 +5,7 @@ import { Observable } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class TicketService {
-  private BASE_URL = 'http://localhost:4000/api';
+  private BASE_URL = 'https://support-ticket-system-q4y8.onrender.com/api';
 
   constructor(private http: HttpClient, private auth: AuthService) {}
   //old code
