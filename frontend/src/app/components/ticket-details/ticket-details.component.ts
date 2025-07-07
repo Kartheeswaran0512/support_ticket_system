f1105c38cd3bbd9372372a771b70909458538b8c
kartheeswaran <karthi@gmail.com>
2025-07-07 17:16:31 +0530

    edit

--- frontend/src/app/components/ticket-details/ticket-details.component.ts
+++ frontend/src/app/components/ticket-details/ticket-details.component.ts
@@ -33,10 +33,12 @@ import { RouterModule } from '@angular/router';
     <div class="min-h-screen bg-gray-900 p-4">
       <div class="max-w-4xl mx-auto">
         <!-- Back Button -->
-        <button mat-stroked-button routerLink="/my-tickets" class=" text-yellow-300 mb-4">
+         <div class=" text-yellow-300">
+        <button mat-stroked-button routerLink="/my-tickets" class=" mb-4">
           <mat-icon>arrow_back</mat-icon>
           Back to My Tickets
         </button>
+         </div>
         
         <!-- Loading State -->
         <div *ngIf="!ticket" class="text-center py-8">
