fd24600cb23a4442cf762ff94628bb2ea90d8a66
kartheeswaran <karthi@gmail.com>
2025-07-07 17:23:30 +0530

    edit

--- frontend/src/app/components/ticket-details/ticket-details.component.ts
+++ frontend/src/app/components/ticket-details/ticket-details.component.ts
@@ -34,10 +34,10 @@ import { RouterModule } from '@angular/router';
       <div class="max-w-4xl mx-auto">
         <!-- Back Button -->
          <div class=" bg-yellow-300">
-        <p mat-stroked-button routerLink="/my-tickets" class=" mb-4">
+        <button mat-stroked-button routerLink="/my-tickets" class=" mb-4">
           <mat-icon>arrow_back</mat-icon>
           Back to My Tickets
-        </p>
+         </button>
         </div>
         
         <!-- Loading State -->
